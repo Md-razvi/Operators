311685816183b9c7bd2a95c881f6b2e1fe6e266b
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-05 01:17:18 +0530

    Add files via upload

---
+++ Problem7.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class Problem7 {
+    public static void main(String[] args)
+    {
+        int a =34,b=67;
+        if(a>b)
+        {
+            System.out.println(a+"Is greater than "+b);
+        }
+        else
+        {
+            System.out.println(a+" less than "+b);
+
+        }
+    }
+}
