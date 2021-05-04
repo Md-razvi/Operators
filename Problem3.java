6fb56442b0b1e527b11919f5d66b30750f842670
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-04 22:57:01 +0530

    Add files via upload

---
+++ Problem3.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class Problem3 {
+    public static void main(String[] args)
+    {
+        int a=23,b=54;
+        //Single = means assignments operator
+        //Double == means equal to operator  
+        // One exclamation and single = means not equal to i.e(!=)
+        System.out.println(a==b);// prints boolean value false
+        System.out.println(a!=b);// prints boolean value True
+        System.out.println(a==23);// prints boolean value True
+
+    }
+    
+}
