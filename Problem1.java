bab7537da6813dec8013af8e7abbc0e029821839
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2021-05-04 21:52:00 +0530

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,18 @@
+package Operators;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Problem1 {
+    public static void main(String[] args)
+    {
+        int a=19,b=8;
+        float c=a/b;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(c);
+
+
+    }
+}
